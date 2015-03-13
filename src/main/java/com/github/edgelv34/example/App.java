07ca58dd5db9d9edd01209065c5d559cfed4de82
jspark <jspark@tlab.co.kr>
2015-03-13 13:57:44 +0900

    xlet-example maven project create

---
+++ src/main/java/com/github/edgelv34/example/App.java
@@ -0,0 +1,13 @@
+package com.github.edgelv34.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
